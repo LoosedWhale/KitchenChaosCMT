43bdb87e48e94f7175b71d14bff3711f37966b2b
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-02-19 09:33:07 +0100

    Refactored the code so that the game input is different from  the player script

--- KitchenChaosCMT/Assets/Scripts/GameInput.cs
+++ KitchenChaosCMT/Assets/Scripts/GameInput.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 GetMovementVectorNormalized(){
+        Vector2 inputVector = new Vector2(0,0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector.y = +1;
+        } 
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector.y = -1;
+        } 
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+        return inputVector;
     }
 }
