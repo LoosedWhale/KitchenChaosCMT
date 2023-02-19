1efaaca53bba4379b7976f8bd67ef060a097ee12
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-02-19 15:03:59 +0100

    Made the kitchen counter be able to understand that it has an object on it. Also added  an SO that later created tomato and Chess Block

---
+++ KitchenChaosCMT/Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public KitchenObjectSO GetKitchenObjectSO() {
+        return kitchenObjectSO;
+    }
+}
